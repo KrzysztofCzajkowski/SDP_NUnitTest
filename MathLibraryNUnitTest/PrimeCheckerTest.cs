bf07a9c18511ac057120e22409f171bb071df5c9
Krzysztof Czajkowski <krzysztof.czajkowski1@o2.pl>
2022-01-31 18:40:59 +0100

    Completed homework.

---
+++ MathLibraryNUnitTest/PrimeCheckerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AutoFixture;
+using FluentAssertions;
+using MathLibrary.MathLibrary;
+using Moq;
+using NUnit.Framework;
+
+namespace MathLibraryNUnitTest
+{
+    public class PrimeCheckerTest
+    {
+        [Test]
+        public void IsPrime_NegativeValue_ThrowsException()
+        {
+            Assert.Throws<ArgumentException>(() => PrimeChecker.IsPrime(-5));
+        }
+
+        [TestCase(0, false)]
+        [TestCase(1, false)]
+        [TestCase(2, true)]
+        [TestCase(3, true)]
+        [TestCase(4, false)]
+        [TestCase(5, true)]
+        [TestCase(6, false)]
+        public void IsPrime_IntegerValue_ReturnsIsPrime(int value, bool expectedValue)
+        {
+            // act
+            var actualValue = PrimeChecker.IsPrime(value);
+
+            // assert
+            actualValue.Should().Be(expectedValue);
+        }
+    }
+}
